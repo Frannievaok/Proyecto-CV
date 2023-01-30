eb5d35becd480d93fcb9c68510785e28f78248f4
Francisco Nieva <nievaf36@gmail.com>
2023-01-29 23:38:32 -0300

    finalizacion de la estructura, agregado de JS

---
+++ index.js
@@ -0,0 +1,26 @@
+let contenedor = document.getElementById("cont-desarollo-web");
+let seccion = document.getElementById("web-development-dropdown");
+
+
+contenedor.addEventListener("click", function(){
+seccion.classList.toggle("visible");
+ })
+
+let contenedor1 = document.getElementById("cont-artiulos");
+let seccion1 = document.getElementById("dropdown-items");
+
+
+contenedor1.addEventListener("click", function(){
+seccion1.classList.toggle("visible");
+ })
+
+let contenedor2 = document.getElementById("cont-estudiante");
+let seccion2 = document.getElementById("student-dropdown");
+
+
+contenedor2.addEventListener("click", function(){
+seccion2.classList.toggle("visible");
+ })
+
+
+  
